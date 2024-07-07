202befd359df79d81fe4f2e79d8344117778187c
aadilnoor <aadilnoor976@gmail.com>
2024-07-07 17:40:35 +0530

    day 1

---
+++ src/com/aamir/aadilnoor/lambda/LambdaApproach.java
@@ -0,0 +1,13 @@
+package com.aamir.aadilnoor.lambda;
+
+interface LambdaInterface{
+	void shape();
+}
+public class LambdaApproach {
+
+	public static void main(String[] args) {
+		
+		LambdaInterface lambdaInterface = () -> System.out.println("Shape is Rectangle");
+		lambdaInterface.shape();
+	}
+}
---
+++ src/com/aamir/aadilnoor/lambda/SingleParameterFI.java
@@ -0,0 +1,15 @@
+package com.aamir.aadilnoor.lambda;
+
+interface SPI{
+	void digit(int number);
+}
+
+public class SingleParameterFI {
+
+	public static void main(String[] args) {
+		
+		SPI spi = (num) -> System.out.println(num);
+		
+		spi.digit(786);
+	}
+}
