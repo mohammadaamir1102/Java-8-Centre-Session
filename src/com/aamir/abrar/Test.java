67f8036f90f49b39b22696c2d3e392c839bc266c
abrar6390 <abrark4961@gmail.com>
2024-07-07 12:02:01 +0530

    Test

---
+++ src/com/aamir/abrar/Test.java
@@ -0,0 +1,7 @@
+package com.aamir.abrar;
+
+public class Test {
+public static void main(String[] args) {
+	
+}
+}
--- src/com/abrar/Test.java
+++
@@ -1,9 +0,0 @@
-package com.abrar;
-
-public class Test {
-
-	public static void main(String[] args) {
-
-	}
-
-}
