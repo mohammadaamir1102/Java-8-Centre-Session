8491c93cefdb824fe9a590b35d2ebb75c61f7fb2
NathuBaba25 <khanmohdhozaifa@gmail.com>
2024-07-07 12:15:54 +0530

    new class

---
+++ src/com/aamir/huzaifa/Retest.java
@@ -0,0 +1,8 @@
+package com.aamir.huzaifa;
+
+public class Retest {
+	public static void main(String[] args) {
+		System.out.println("Retest");
+	}
+
+}
---
+++ src/com/aamir/huzaifa/Test.java
@@ -0,0 +1,9 @@
+package com.aamir.huzaifa;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println("Bismillah");
+	}
+
+}
