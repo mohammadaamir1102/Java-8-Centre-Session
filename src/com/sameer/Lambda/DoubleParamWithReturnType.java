4a1e309b5efdca24db218bb3a1c5be39ed4a7f76
sameer-siddiqui <140159169+sameersiddiqui1240@users.noreply.github.com>
2024-07-08 19:15:42 +0530

    java 8

--- src/com/sameer/Lambda/DoubleParamWithReturnType.java
+++ src/com/sameer/Lambda/DoubleParamWithReturnType.java
@@ -1,15 +1,15 @@
 package com.sameer.Lambda;
 
-interface Inputs{
-	int sum(int num1 , int num2);
+interface Inputs {
+	int sum(int num1, int num2);
 }
 
 public class DoubleParamWithReturnType {
 
 	public static void main(String[] args) {
-		
-		Inputs inputs = (num1 , num2) ->  num1+num2;
-		System.out.println(inputs.sum(10,29));
-		
+
+		Inputs inputs = (num1, num2) -> num1 + num2;
+		System.out.println(inputs.sum(10, 29));
+
 	}
 }
