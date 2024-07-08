e302371722829d29ae3e899faf9799734a951ba0
Suhail Ahmad <ksuhail014@gmail.com>
2024-07-08 20:26:53 +0530

    new

--- src/com/Huzaifa/Test.java
+++
@@ -1,5 +0,0 @@
-package com.Huzaifa;
-
-public class Test {
-
-}
--- src/com/aamir/suhail/Test.java
+++ src/com/aamir/suhail/Test.java
@@ -1,9 +1,8 @@
-package com.aamir.Tufail;
+package com.aamir.suhail;
 
 public class Test {
-	
 	public static void main(String[] args) {
-		System.out.println("Bismillahs");
+		System.out.println("hello");
 	}
 
 }
