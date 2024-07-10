de7d9ccdccdf3a177d30ce443312d4f373c60412
khanahmad-tech <khanahmadhusain30@gmail.com>
2024-07-10 10:25:14 +0530

    new

--- src/com/aamir/ahmad/ReTest.java
+++ src/com/aamir/ahmad/ReTest.java
@@ -1,10 +1,10 @@
 package com.aamir.ahmad;
 
-public class Test {
+public class ReTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test");
+		System.out.println("ReTest");
 
 	}
 
