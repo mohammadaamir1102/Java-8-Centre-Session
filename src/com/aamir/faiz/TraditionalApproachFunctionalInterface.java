f445e332e647df557547fd62302c0fcbc00f231a
khanfaiz1609 <khanfaiztech1609@gmail.com>
2024-07-10 09:51:51 +0530

    Change

--- src/com/aamir/faiz/TraditionalApproachFunctionalInterface.java
+++ src/com/aamir/faiz/TraditionalApproachFunctionalInterface.java
@@ -3,14 +3,14 @@ package com.aamir.faiz;
 interface TradiAppro{
 	void test();
 }
-public class Test1 implements TradiAppro{
+public class TraditionalApproachFunctionalInterface implements TradiAppro{
 
 	@Override
 	public void test() {
 		System.out.println("hello Traditional Approach");
 	}
 	public static void main(String[] args) {
-		Test1 test = new Test1();
+		TraditionalApproachFunctionalInterface test = new TraditionalApproachFunctionalInterface();
 		test.test();
 	}
 
