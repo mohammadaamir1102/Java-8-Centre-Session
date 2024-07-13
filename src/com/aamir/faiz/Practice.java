cf15272c2905da9bee4490bb2fc8c2d99eb356ee
khanfaiz1609 <khanfaiztech1609@gmail.com>
2024-07-13 15:58:51 +0530

    f

--- src/com/aamir/faiz/Practice.java
+++ src/com/aamir/faiz/Practice.java
@@ -1,17 +1,32 @@
 package com.aamir.faiz;
 
+import java.util.Scanner;
 import java.util.function.BiPredicate;
 
 public class Practice {
 	public static void main(String[] args) {
-		BiPredicate<Integer, Integer> biPredicate=(hell,hell1)->{
-			if (hell>hell1) {
-				return true;	
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter First Number : ");
+		int a = scanner.nextInt();
+
+		System.out.println("Enter Second Number : ");
+		int b = scanner.nextInt();
+
+		BiPredicate<Integer, Integer> biPredicate = (hell, hell1) -> {
+			if (hell > hell1) {
+				return true;
 			}
 			throw new RuntimeException("Exception:- I am Back!!!.........");
-			
-		};
-		System.out.println(biPredicate.test(0,20));
-	}
 
+		};
+		try {
+			System.out.println(biPredicate.test(a, b));
+		} catch (RuntimeException e) {
+			System.out.println(e.getMessage());
+
+		}
+		scanner.close();
+	}
 }
