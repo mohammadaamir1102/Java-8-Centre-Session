db6987f4c2a4607d3e895b04bc85de8eefdfa230
khanfaiz1609 <khanfaiztech1609@gmail.com>
2024-07-15 18:45:30 +0530

    Task1407

---
+++ src/com/aamir/faiz/Task1407/PredicateCheckIfNumbersInArrayListEvenOrNot.java
@@ -0,0 +1,20 @@
+package com.aamir.faiz.Task1407;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateCheckIfNumbersInArrayListEvenOrNot {
+
+	public static void main(String[] args) {
+		List<Integer> evenList = evenList();
+		for (Integer integer : evenList) {
+			Predicate<List<?>> checkEvenOrNot = value -> integer % 2 == 0;
+			System.out.println(checkEvenOrNot.test(evenList));
+		}
+	}
+
+	public static List<Integer> evenList() {
+		return List.of(2, 4, 6, 8);
+	}
+
+}
