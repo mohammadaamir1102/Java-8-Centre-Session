9004485641960fe97686eb4f8ed8ad68433245e4
ansarulhaque9554 <ansarulhaque97019@gmail.com>
2024-07-19 20:41:20 +0530

    first commit

--- src/com/aamir/aadilnoor/ReTest.java
+++
@@ -1,7 +0,0 @@
-package com.aamir.aadilnoor;
-
-public class ReTest {
-public static void main(String[] args) {
-	System.out.println("Re Test ");
-}
-}
--- src/com/aamir/mushahid/Test1.java
+++
@@ -1,8 +0,0 @@
-package com.aamir.mushahid;
-
-public class Test1 {
-	public static void main(String[] args) {
-		System.out.println("Test 2");
-	}
-
-}
