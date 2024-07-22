af2b05da7c2e7fa7534751d0ad733b7b1bb641d4
NathuBaba25 <khanmohdhozaifa@gmail.com>
2024-07-22 23:50:10 +0530

    Comment added

--- src/com/aamir/huzaifa/BipredicateEx6.java
+++ src/com/aamir/huzaifa/BipredicateEx6.java
@@ -5,7 +5,7 @@ public class BipredicateEx6 {
 	
 	    public static void main(String[] args) {
 	        BiPredicate<int[], int[]> areArrayLengthsEqual = (arr1, arr2) -> arr1.length == arr2.length;
-
+			// this task is given by Aamir
 	      System.out.println(areArrayLengthsEqual.test(new int[]{1, 2, 3}, new int[]{4, 5, 6})); 
 	      System.out.println(areArrayLengthsEqual.test(new int[]{1, 2}, new int[]{4, 5, 6}));
 	    }
