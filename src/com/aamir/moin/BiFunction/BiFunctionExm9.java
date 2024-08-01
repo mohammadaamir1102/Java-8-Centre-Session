61c6d9211acc062259551399b4565590afe9d68f
AbdulMoyeen <moyeenkhan47@gmail.com>
2024-08-01 19:41:01 +0530

    final commit

--- src/com/aamir/moin/BiFunction/BiFunctionExm9.java
+++ src/com/aamir/moin/BiFunction/BiFunctionExm9.java
@@ -1,7 +1,18 @@
 package com.aamir.moin.BiFunction;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.function.BiFunction;
+
 public class BiFunctionExm9 {
     public static void main(String[] args) {
+        BiFunction<LocalDate, LocalDate, Integer> calculateAge = (birthDate, currentDate) ->
+                Period.between(birthDate, currentDate).getYears();
 
+
+        LocalDate birthDate = LocalDate.of(2005, 5, 12);
+        LocalDate currentDate = LocalDate.now();
+        int age = calculateAge.apply(birthDate, currentDate);
+        System.out.println("Age: " + age);
     }
 }
